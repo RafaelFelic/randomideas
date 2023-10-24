528d8fc5655488b5a64a3086c2e0491882b90dbf
RafaelFelic <rafaelfelic@gmail.com>
2023-10-24 22:00:43 +1000

    Fullstack updated

---
+++ client/webpack.config.js
@@ -0,0 +1,55 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, '../public'),
+    filename: 'bundle.js',
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, '../public'),
+    },
+    port: 3000,
+    open: {
+      app: {
+        name: 'chrome',
+      },
+    },
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+    proxy: {
+      '/api': 'http://localhost:5000',
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Webpack App',
+      filename: 'index.html',
+      template: './src/index.html',
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+};
